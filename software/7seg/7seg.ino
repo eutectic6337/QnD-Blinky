a9096803bfb44165a78d4c4bffd3b79f4df711d2
Mark L Pappin <mlp@acm.org>
2023-07-07 16:27:44 -0500

    change map implementation to direct table lookup

--- software/7seg/7seg.ino
+++ software/7seg/7seg.ino
@@ -1,11 +1,5 @@
 const char message[] = "dc615 ... dEFcon31   ";
 
-const char alphabet[] =
-  "0123456789"
-  "._-~=\|/?[]"
-  "abcdefghijklmnopqrstuvwxyz"
-  "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
 /* 7seg
 
   A  
@@ -23,85 +17,101 @@ const char alphabet[] =
 #define dp 0x80
 #define unavailable A|D|G
 
-const unsigned map[] =
-//"0123456789"
-  A|B|C|D|E|F,  //0
-  B|C,//1
-  A|B|D|E|G,//2
-  A|B|C|D|G,//3
-  B|C|F|G,//4
-  A|C|D|F,//5
-  A|C|D|E|F|G,//6
-  A|B|C,//7
-  A|B|C|D|E|F|G,//8
-  A|B|C|D|F|G,//9
-//"._-~=\|/?[]"
-  dp,//.
-  D,//_
-  G,//-
-  A,//~
-  D|G,//=
-  B|E|G,//backslash
-  E|F,//|
-  F|G|C,///
-  A|B|E|G,//?
-  A|D|E|F,//[
-  A|B|C|D,//]
-//"abcdefghijklmnopqrstuvwxyz"
-  unavailable,//a
-  C|D|E|F|G,//b
-  D|E|G,//c
-  B|C|D|E|G,//d
-  A|B|C|D|F|G,//e
-  A|E|F|G,//f
-  unavailable,//g 9
-  C|E|F|G,//h
-  C,//i
-  unavailable,//j
-  unavailable,//k
-  E|F,//l
-  unavailable,//m
-  C|E|G,//n
-  C|D|E|G,//o
-  unavailable,//p P
-  A|B|C|F|G,//q
-  E|G,//r
-  unavailable,//s 5
-  unavailable,//t
-  C|D|E,//u
-  unavailable,//v u
-  unavailable,//w
-  unavailable,//x H
-  B|C|D|F|G,//y
-  unavailable,//z 2
-//"ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  A|B|C|E|F|G,//A
-  unavailable,//B 8
-  unavailable,//C [
-  unavailable,//D 0
-  A|D|E|F|G,//E
-  A|E|F|G,//F
-  unavailable,//G
-  B|C|E|F|G,//H
-  unavailable,//I l,1
-  B|C|D|E,//J
-  unavailable,//K
-  D|E|F,//L
-  unavailable,//M
-  unavailable,//N
-  unavailable,//O 0
-  A|B|E|F|G,//P
-  unavailable,//Q
-  unavailable,//R A
-  unavailable,//S 5
-  unavailable,//T
-  A|B|C|D|E|F,//U
-  unavailable,//V U
-  unavailable,//W
-  unavailable,//X H
-  unavailable,//Y
-  unavailable,//Z 2
+const struct {
+  char c;
+  unsigned char segments;
+} raw_7segment_map[] =
+  {'0', A|B|C|D|E|F},
+  {'1', B|C},
+  {'2',A|B|D|E|G},
+  {'3',A|B|C|D|G},
+  {'4',B|C|F|G},
+  {'5',A|C|D|F},
+  {'6',A|C|D|E|F|G},
+  {'7',A|B|C},
+  {'8',A|B|C|D|E|F|G},
+  {'9',A|B|C|D|F|G},
+
+  {'.',dp},
+  {'_',D},
+  {'-',G},
+  {'~',A},
+  {'=',D|G},
+  {'\\',B|E|G},
+  {'|',E|F},
+  {'/',F|G|C},
+  {'?',A|B|E|G},
+  {'[',A|D|E|F},
+  {']',A|B|C|D},
+
+  {'a',unavailable},
+  {'b',C|D|E|F|G},
+  {'c',D|E|G},
+  {'d',B|C|D|E|G},
+  {'e',A|B|C|D|F|G},
+  {'f',A|E|F|G},
+  {'g 9',unavailable},
+  {'h',C|E|F|G},
+  {'i',C},
+  {'j',unavailable},
+  {'k',unavailable},
+  {'l',E|F},
+  {'m',unavailable},
+  {'n',C|E|G},
+  {'o',C|D|E|G},
+  {'p P',unavailable},
+  {'q',A|B|C|F|G},
+  {'r',E|G},
+  {'s 5',unavailable},
+  {'t',unavailable},
+  {'u',C|D|E},
+  {'v u',B|F|G},
+  {'w',unavailable},
+  {'x H',unavailable},
+  {'y',B|C|D|F|G},
+  {'z 2',unavailable},
+
+  {'A',A|B|C|E|F|G},
+  {'B 8',unavailable},
+  {'C [',unavailable},
+  {'D 0',unavailable},
+  {'E',A|D|E|F|G},
+  {'F',A|E|F|G},
+  {'G',unavailable},
+  {'H',B|C|E|F|G},
+  {'I l,1',unavailable},
+  {'J',B|C|D|E},
+  {'K',unavailable},
+  {'L',D|E|F},
+  {'M',unavailable},
+  {'N',unavailable},
+  {'O 0',A|B|F|G},
+  {'P',A|B|E|F|G},
+  {'Q',unavailable},
+  {'R A',unavailable},
+  {'S 5',unavailable},
+  {'T',unavailable},
+  {'U',A|B|C|D|E|F},
+  {'V U',unavailable},
+  {'W',unavailable},
+  {'X H',unavailable},
+  {'Y',unavailable},
+  {'Z',unavailable},
 };
+
+unsigned char cached_7segment_map[256];
+void populate_7segment_map(void)
+{
+  for (int i= 0; i<sizeof(rawmap)/sizeof(raw_7segment_map[0]); i++) {
+    cached_7segment_map[raw_7segment_map[i].c] = raw_7segment_map[i].segments;
+  }
+}
+unsigned char map(char c)
+{
+  return cached_7segment_map[(unsigned char)c];
+}
+
+
 #undefine A
 #undefine B
 #undefine C
@@ -113,6 +123,7 @@ const unsigned map[] =
 
 void setup() {
   // put your setup code here, to run once:
+  populate_7segment_map();
 
 }
 
